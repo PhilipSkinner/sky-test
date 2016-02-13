73acb9e069a002ff20b4ef7144340bb81f28cc43
Philip Skinner <me@philip-skinner.co.uk>
2016-02-13 10:59:27 +0000

    some work on connecting to a data source

--- main.js
+++ main.js
@@ -4,11 +4,21 @@
 */
 
 var express     = require('express');
+var q           = require('q');
+var mysql       = require('./node/db');
 
 //might want to set this via config or args later
 var port        = 8080;
 var debug       = true;
 var app         = express();
+
+//connect to our database
+mysql.db.initialize({
+    hostname    : 'localhost',
+    database    : 'sky',
+    username    : 'sky',
+    password    : 'sky',
+})
 
 //enable static serving
 app.use(express.static('site'));
@@ -18,7 +28,35 @@ app.get('/api/v1/confirm', function(req, res) {
 });
 
 app.get('/api/v1/customer/location', function(req, res) {
-    res.send("1");
+    var customerID = null;        
+    
+    customerID = req.query.customerID ? req.query.customerID : customerID;
+    customerID = req.query.customerId ? req.query.customerId : customerID;
+    customerID = req.query.customerid ? req.query.customerid : customerID;
+    
+    if (customerID == null) {
+        res.jsonp({
+            meta    : {
+                code    : 500,
+                message : 'Missing customerID argument',                
+            },            
+        });
+        return;
+    }
+    
+    //is the user valid?
+    q.when(mysql.db.fetchCustomer(customerID))
+    .then(function(value) {
+        console.log(value);
+    }.bind({ res : res, req : req }), function(error) {
+        this.res.jsonp({
+            meta    : {
+                code    : 500,
+                message : 'Unknown database error occured.',  
+            },
+        });
+        return;
+    }.bind({ res : res, req : req }));        
 });
 
 app.get('/api/v1/catalogue', function(req, res) {
---
+++ node/db.js
@@ -0,0 +1,28 @@
+var mysql = require('mysql');
+var q = require('q');
+
+//no ORM, just quick
+var db = {
+    connection  : null,
+    
+    initialize: function(params) {
+        this.connection = mysql.createConnection({
+            host        : params.hostname,
+            database    : params.database,
+            user        : params.username,
+            password    : params.password,
+        });
+        this.connection.connect();
+        
+        //init our methods now
+        this.initMethods();        
+    },
+        
+    initMethods : function() {
+        this.fetchCustomer = q.nbind(this.connection.query, this.connection, "SELECT * FROM customer WHERE uid=?");        
+    },    
+}
+
+module.exports = {
+    db : Object.create(db),  
+};
