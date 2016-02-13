5bb5af68c2b7d5a9e08afe309ea8a9cba5d62a9d
Philip Skinner <me@philip-skinner.co.uk>
2016-02-13 12:11:24 +0000

    some simple tests, when I swap over to stubs we will ensure that there are tests checking for the expected value & structure etc

--- spec/api_v1_catalogue_spec.js
+++ spec/api_v1_catalogue_spec.js
@@ -1,25 +1,96 @@
+var request = require('request');
+var base = 'http://localhost:8080/api/v1/catalogue';
+var app = require('../main');
+
 describe("API::V1 Catalogue endpoint test", function() {    
     describe("GET /api/v1/catalogue", function() {
-        it ("returns 200", function() {
-            
+        it ("returns 200", function(done) {
+            request.get(base, function(error, response, body) {
+                expect(response).not.toBeNull();
+                expect(response).toBeDefined();
+                if (response) {
+                    expect(response.statusCode).toBe(200);
+                } 
+                done();
+            });
+        });
+        
+        it("checks for null locationID", function(done) {
+            request.get(base, function(error, response, body) {
+                expect(body).not.toBeNull();                
+                expect(body).toBeDefined();
+                                                
+                if (body) {
+                    body = JSON.parse(body);
+                    expect(body.meta).toBeDefined();
+                    
+                    expect(body.meta.message).toBeDefined();
+                    expect(body.meta.message).toBe("Missing locationID argument");
+                    
+                    expect(body.meta.code).toBeDefined();
+                    expect(body.meta.code).toBe(500);
+                }
+                done();
+            });
+        });
+        
+        it("checks for invalid locationID", function(done) {
+            request.get(base + '?locationID=sillyvalue', function(error, response, body) {
+                expect(body).not.toBeNull();
+                expect(body).toBeDefined();
+                
+                if (body) {
+                    body = JSON.parse(body);
+                    expect(body.meta).toBeDefined();
+                    
+                    expect(body.meta.message).toBeDefined();
+                    expect(body.meta.message).toBe("No such location found");
+                    
+                    expect(body.meta.code).toBeDefined();
+                    expect(body.meta.code).toBe(404);
+                }
+                
+                done();
+            });
         });
     });
     
     describe("POST /api/v1/catalogue", function() {
-        it ("returns 404", function() {
-            
+        it ("returns 404", function(done) {
+            request.post(base, function(error, response, body) {
+                expect(response).not.toBeNull(); 
+                expect(response).toBeDefined();                          
+                if (response) {
+                    expect(response.statusCode).toBe(404);                    
+                }                               
+                done();
+            });
         });
     });
     
     describe("DELETE /api/v1/catalogue", function() {
-        it ("returns 404", function() {
-            
+        it ("returns 404", function(done) {
+            request.del(base, function(error, response, body) {
+                expect(response).not.toBeNull(); 
+                expect(response).toBeDefined();                          
+                if (response) {
+                    expect(response.statusCode).toBe(404);                    
+                }                               
+                done();
+            });
         }); 
     });
     
     describe("PUT /api/v1/catalogue", function() {
-         it ("returns 404", function() {
-             
+         it ("returns 404", function(done) {
+             request.put(base, function(error, response, body) {
+                expect(response).not.toBeNull(); 
+                expect(response).toBeDefined();                          
+                if (response) {
+                    expect(response.statusCode).toBe(404);                    
+                }                               
+                done();
+            });
          });
     });  
 });
